78f04ddffa4ce6f81733f3686f34bf68a87b52d7
peterridingdev <peterdriding@gmail.com>
2026-01-20 14:42:44 +0400

    refactor(experience): convert to TypeScript with strict typing

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -5,50 +5,47 @@ import Camera from "./Camera";
 import Renderer from "./Renderer";
 import World from "./World/World";
 
-let instance = null;
+let instance: Experience | null = null;
+
+declare global {
+  interface Window {
+    experience: Experience;
+  }
+}
 
 export default class Experience {
-  constructor(canvas) {
-    // Singleton (make only one instance of experience when delcaring a new one especially important as it acts as the controller)
-    if (instance) {
-      return instance;
-    }
+  canvas!: HTMLCanvasElement;
+  sizes!: Sizes;
+  time!: Time;
+  scene!: THREE.Scene;
+  camera!: Camera;
+  renderer!: Renderer;
+  world!: World;
+
+  constructor(canvas: HTMLCanvasElement) {
+    if (instance) return instance;
     instance = this;
 
-    // Global access
     window.experience = this;
 
-    // Options
     this.canvas = canvas;
-
-    // Setup
     this.sizes = new Sizes();
-    console.log(this);
     this.time = new Time();
     this.scene = new THREE.Scene();
     this.camera = new Camera();
-
-    // Renderer
     this.renderer = new Renderer();
-
-    // World
     this.world = new World();
 
-    // Resize event
-    this.sizes.on("resize", () => {
-      this.resize();
-    });
-
-    // Time tick event
-    this.time.on("tick", () => {
-      this.update();
-    });
+    this.sizes.on("resize", () => this.resize());
+    this.time.on("tick", () => this.update());
   }
-  resize() {
+
+  resize(): void {
     this.camera.resize();
     this.renderer.resize();
   }
-  update() {
+
+  update(): void {
     this.camera.update();
     this.renderer.update();
   }
